ccb59d3a483c46229001202453823b0ee1e136a6
Santiago Rangel <s.rangel@uniandes.edu.co>
2018-03-20 09:21:38 -0500

    credenciales

--- Sistrans/src/rest/HabitacionesService.java
+++ Sistrans/src/rest/HabitacionesService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -48,4 +49,20 @@ public class HabitacionesService {
 			return Response.status(500).entity(doErrorMessage(e)).build();
 		}
 	}
+//	@GET
+//	@Path( "{id: \\d+}" )
+//	@Produces( { MediaType.APPLICATION_JSON } )
+//	public Response getBebedorById( @PathParam( "id" ) Long id )
+//	{
+//		try{
+//			AlohaTM tm = new AlohaTM( getPath( ) );
+//			
+//			Bebedor bebedor = tm.getBebedorById( id );
+//			return Response.status( 200 ).entity( bebedor ).build( );			
+//		}
+//		catch( Exception e )
+//		{
+//			return Response.status( 500 ).entity( doErrorMessage( e ) ).build( );
+//		}
+//	}
 }
